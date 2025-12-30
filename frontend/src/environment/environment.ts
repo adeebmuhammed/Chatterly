30ec6ec7dfe899a9c3427bc6f47fd246f4ab080a
adeebmuhammed <muhammedadeeb445@gmail.com>
2025-12-30 11:15:20 +0530

    modified the api url in the frontend environment

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:4000/api',
-  baseUrl: 'http://localhost:4000',
+  apiBaseUrl: 'https://chatterly-nqsj.onrender.com/api',
+  baseUrl: 'https://chatterly-nqsj.onrender.com',
   VAPID_PUBLIC_KEY: 'BPtf7cjwn7TQRZgiUTIyQgNCQKwY0fr1dWpy6fWLB76YFJI2cCDhb5OKCg-PUoyoW01X2U_22jH6uK-tmybeGFg'
 };
